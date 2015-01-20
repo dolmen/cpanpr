2a7461751b9cc29e49592d5e77068e6e57b44c4e
Olivier Mengué <dolmen@cpan.org>
2015-01-20 22:52:57 +0100

    CPANPR::load: simplify parsing; return a hash

--- lib/CPANPR.pm
+++ lib/CPANPR.pm
@@ -11,15 +11,17 @@ sub load ($)
     my $content = do { local $/; <$f> };
     close $f;
 
-    [
+    +{
 	map {
-	    my %row;
-	    @row{qw<member.url member.name dist.url dist.name author.url author.name repo.url repo.path>} =
-		m!<td><a href='([^']*)'>([^>]*)</a></td>!g;
-	    \%row
+	    #my %row;
+	    #@row{qw<member.url member.name dist.url dist.name author.url author.name repo.url repo.path>} =
+	    my @row = m!<td><a href='[^']*'>([^>]*)</a></td>!g;
+	    my %record;
+	    @record{qw<dist author repo>} = @row[1..3];
+	    ($row[0] => \%record)
 	}
 	($content =~ m!<tr>(<td>\V*</td>)</tr>!g)
-    ]
+    }
 }
 
 1;
